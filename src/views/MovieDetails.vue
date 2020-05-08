a0dc0bda72e1c28d22721a52a8ff2a9f4ac8f379
Hans Tanawi <hans.tanawi@outlook.com>
2020-05-08 15:33:54 +0800

    Setting up the basic routes

---
+++ src/views/MovieDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="details">
+    <h1>Movie Details Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieDetails',
+};
+</script>
+
+<style>
+
+</style>
